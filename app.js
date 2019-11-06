34691f27bdb1228f03fabd880facd0f680652464
rufunt <jazzoo@mail.ru>
2019-11-06 20:37:39 +0300

    Add heal and giveup methods

--- app.js
+++ app.js
@@ -27,10 +27,15 @@ new Vue({
             this.monsterAttacks()          
         },
         heal: function () {
-            
+            if (this.playerHealth <= 90) {
+                this.playerHealth += 10
+            }else {
+                this.playerHealth = 100
+            }
+            this.monsterAttacks()
         },
         giveUp: function() {
-
+            this.gameIsRunning = false
         },
         monsterAttacks: function() {
             this.playerHealth -= this.calculateDamage(5,12)
